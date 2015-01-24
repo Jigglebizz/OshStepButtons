130b9b503e67bc904b667e3f0e9edf57cc221eeb
unknown <rustmik2@gmail.com>
2015-01-24 16:24:18 -0500

    Fixed compiler errors

--- OshStepButtons.cpp
+++ OshStepButtons.cpp
@@ -4,10 +4,10 @@
 #include <CapacitiveSensor.h>
 
 OshStepButtons::OshStepButtons() {
-    sensors = (CapacitiveSensor*)malloc(sizeof(CapacitiveSensor * 24));
+    sensors = (CapacitiveSensor**)malloc(sizeof(CapacitiveSensor) * 24);
     for (int i = 0; i < 24; i++) {
         sensors[i] = new CapacitiveSensor(pulse_pin, pins_start + i);
-        sensors[i].set_CS_AutocaL_Millis(0xFFFFFFFF);
+        sensors[i]->set_CS_AutocaL_Millis(0xFFFFFFFF);
     }
 }
 
@@ -15,11 +15,16 @@ OshStepButtons::~OshStepButtons() {
     for (int i = 0; i < 24; i++) {
         delete sensors[i];
     }
-    free sensors;
+    free(sensors);
 }
 
 bool OshStepButtons::getStep(uint8_t step) {
-    long sample_time = CapacitiveSensor[step].capacitiveSensor(30);
+    long sample_time = sensors[step]->capacitiveSensor(30);
+#ifdef DEBUG
+    Serial.print(step);
+    Serial.print(":\t");
+    Serial.println(sample_time);
+#endif
     if (sample_time > threshold)
         return true;
     return false;
--- OshStepButtons.h
+++ OshStepButtons.h
@@ -6,20 +6,20 @@
 
 class OshStepButtons {
   private:
-    static const int pulse_pin      = 11;
-    static const int pins_start     = 30;
-    static const int play_index     = 16;
-    static const int record_index   = 17;
-    static const int shift_index    = 18;
-    static const int yes_index      = 19;
-    static const int up_index       = 20;
-    static const int down_index     = 21;
-    static const int right_index    = 22;
-    static const int left_index     = 23;
+    static const uint8_t pulse_pin      = 11;
+    static const uint8_t pins_start     = 30;
+    static const uint8_t play_index     = 16;
+    static const uint8_t record_index   = 17;
+    static const uint8_t shift_index    = 18;
+    static const uint8_t yes_index      = 19;
+    static const uint8_t up_index       = 20;
+    static const uint8_t down_index     = 21;
+    static const uint8_t right_index    = 22;
+    static const uint8_t left_index     = 23;
 
-    static const int threshold = 400;
+    static const uint8_t threshold = 400;
 
-    CapacitiveSensor* sensors;
+    CapacitiveSensor** sensors;
   public:
     OshStepButtons();
     ~OshStepButtons();
